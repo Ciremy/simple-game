4a56cf19a67a739140d12740423ca607ed6c7f13
ALAVAUD <aymeric.lavaud@ynov.com>
2022-10-21 09:21:47 +0200

    changing lib to use udp

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-hello
+#Protocol explaination
+
+##Protocol
+
+The protocol is a simple protocol that is used to communicate between the client and the server. The protocol is based on an string that is sent to the server. The main goal of the protocol is to be as optimized as possible.
+The Format is :
+
+```js
+`${dataSizeInChar},${typeOfData},${date},${data}`
+//so for exemple for moving up:
+4,m,2022-10-20T18:38:43.686Z,h
+```
+
+In data type, there is 3 types of data :
+m for movement
+f for fire
+i for interaction
+(some will be added later)
--- server.js
+++ server.js
@@ -1,32 +1,54 @@
-const net = require("net");
-var server = net.createServer();
-server.on("connection", (socket) => {
+var udp = require("dgram");
+
+// --------------------creating a udp server --------------------
+
+// creating a udp server
+var server = udp.createSocket("udp4");
+
+// emits when any error occurs
+server.on("error", function (error) {
+  console.log("Error: " + error);
+  server.close();
+});
+
+// emits on new datagram msg
+server.on("message", function (msg, info) {
+  console.log("Data received from client : " + msg.toString());
   console.log(
-    "new client connection is made",
-    socket.remoteAddress + ":" + socket.remotePort
+    "Received %d bytes from %s:%d\n",
+    msg.length,
+    info.address,
+    info.port
   );
-  socket.on("data", (data) => {
-    console.log(data.toString());
-  });
-  socket.once("close", () => {
-    console.log("client connection closed.");
-  });
-  socket.on("error", (err) => {
-    console.log("client connection got errored out.");
-  });
-  socket.write("SERVER: Hello! Connection successfully made.<br>");
+
+  //sending msg
+  // server.send(msg, info.port, "localhost", function (error) {
+  //   if (error) {
+  //     client.close();
+  //   } else {
+  //     console.log("Data sent !!!");
+  //   }
+  // });
 });
-server.on("error", (e) => {
-  if (e.code === "EADDRINUSE") {
-    console.log("Address in use, retrying...");
-    setTimeout(() => {
-      server.close();
-      server.listen(PORT, HOST);
-    }, 1000);
-  } else {
-    console.log("Server failed.");
-  }
+
+//emits when socket is ready and listening for datagram msgs
+server.on("listening", function () {
+  var address = server.address();
+  var port = address.port;
+  var family = address.family;
+  var ipaddr = address.address;
+  console.log("Server is listening at port" + port);
+  console.log("Server ip :" + ipaddr);
+  console.log("Server is IP4/IP6 : " + family);
 });
-server.listen(6969, () => {
-  console.log("opened server on %j", server.address().port);
+
+//emits after the socket is closed using socket.close();
+server.on("close", function () {
+  console.log("Socket is closed !");
 });
+
+server.bind(2222);
+
+// setTimeout(function () {
+//   server.close();
+// }, 8000);
